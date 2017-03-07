130d9605b79367b49c92f8189f2357b24ec96a3b
Sylvia Wu <sylvia.wu@mail.mcgill.ca>
2017-03-07 14:05:36 -0500

    enabled dry

--- Lab3_STM32F4Cube_Base_project/Sources/accelerometer.c
+++ Lab3_STM32F4Cube_Base_project/Sources/accelerometer.c
@@ -1,6 +1,7 @@
 #include "lis3dsh.h"
 #include "stm32f4xx_hal.h"
 #include <math.h>
+#include "accelerometer.h"
 
 #define PI ((float)(3.141592653589793))
 #define RAD_TO_DEG(x) ((x)*((float)180.0) / PI)
@@ -12,17 +13,17 @@ LIS3DSH_DRYInterruptConfigTypeDef LIS3DSH_DRY;
 void initialize_accel(void)
 {	
 	/* LIS3DSH struct */
-	LIS3DSH_InitType.Power_Mode_Output_DataRate = LIS3DSH_DATARATE_25; //Output data 25 times per second (frequency of 25Hz)
-	LIS3DSH_InitType.Axes_Enable = LIS3DSH_XYZ_ENABLE; //Enable x, y, and z axes
+	LIS3DSH_InitType.Power_Mode_Output_DataRate = LIS3DSH_DATARATE_25; // Output data 25 times per second (frequency of 25Hz)
+	LIS3DSH_InitType.Axes_Enable = LIS3DSH_XYZ_ENABLE; // Enable x, y, and z axes
 	LIS3DSH_InitType.Continous_Update = LIS3DSH_ContinousUpdate_Enabled; //
 	LIS3DSH_InitType.AA_Filter_BW = LIS3DSH_AA_BW_50; //
-	LIS3DSH_InitType.Full_Scale = LIS3DSH_FULLSCALE_2; //Full scale of +/- 2g by default
-	LIS3DSH_InitType.Self_Test = LIS3DSH_SELFTEST_NORMAL; //Normal mode by default
+	LIS3DSH_InitType.Full_Scale = LIS3DSH_FULLSCALE_2; // Full scale of +/- 2g by default
+	LIS3DSH_InitType.Self_Test = LIS3DSH_SELFTEST_NORMAL; // Normal mode by default
 	
 	LIS3DSH_Init(&LIS3DSH_InitType);
 	
 	/* LIS3DSH Data ready interrupt struct */
-	LIS3DSH_DRY.Dataready_Interrupt = LIS3DSH_DATA_READY_INTERRUPT_DISABLED; //Enable DRY interrupt
+	LIS3DSH_DRY.Dataready_Interrupt = LIS3DSH_DATA_READY_INTERRUPT_DISABLED; // Enable DRY interrupt
   LIS3DSH_DRY.Interrupt_signal = LIS3DSH_ACTIVE_HIGH_INTERRUPT_SIGNAL; // Interrupt Signal Active High (requirements)
   LIS3DSH_DRY.Interrupt_type = LIS3DSH_INTERRUPT_REQUEST_PULSED; // Interrupt type to be pulsed (requirements)
 	
@@ -33,7 +34,7 @@ void initialize_accel(void)
 // Read values from accelerometer
 void reading_accel_values(float *ax, float *ay, float *az)
 {
-	float acc[3]; //empty array to store the acceleration values
+	float acc[3]; // Empty array to store the acceleration values
 	
 	LIS3DSH_ReadACC(acc);
 	*ax = acc[0];
