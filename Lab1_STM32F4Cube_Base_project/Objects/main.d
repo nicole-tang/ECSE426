78cdb6f8292f6cc3a0fe3f2d78ad8a635797ccc4
nicole-tang <nicole.tang@mail.mcgill.ca>
2017-03-14 16:23:29 -0400

    added lab 4

--- Lab1_STM32F4Cube_Base_project/Objects/main.d
+++
@@ -1,3 +0,0 @@
-.\objects\main.o: Sources\main.c
-.\objects\main.o: C:\McGill\Software_Install\EDU\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\main.o: C:\McGill\Software_Install\EDU\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
