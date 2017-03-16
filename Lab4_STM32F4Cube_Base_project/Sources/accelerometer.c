5e361bb5c54a7e02b11a7ebde62db31e51ad3579
Sylvia Wu <sylvia.wu@mail.mcgill.ca>
2017-03-16 14:39:21 -0400

    Minor changes

--- Lab4_STM32F4Cube_Base_project/Sources/accelerometer.c
+++ Lab4_STM32F4Cube_Base_project/Sources/accelerometer.c
@@ -2,10 +2,47 @@
 #include "stm32f4xx_hal.h"
 #include "accelerometer.h"
 #include "gpio.h"
+#include "cmsis_os.h"  
+
+// Variables
+float acc[3] = {0, 0, 0};
+int pitch_output = 0;
+int roll_output = 0; 
 
 
 LIS3DSH_InitTypeDef LIS3DSH_InitType;
 LIS3DSH_DRYInterruptConfigTypeDef LIS3DSH_DRY;
+
+
+// Thread declaration
+void Thread_acceleration(void const *argument);
+osThreadId tid_Thread_acceleration; // Thread ID
+osThreadDef(Thread_acceleration, osPriorityAboveNormal, 1, 800); // Thread name, priority, instances, stack size
+
+
+// Start accelerometer thread
+int start_Thread_acceleration(void)
+{
+	tid_Thread_acceleration = osThreadCreate(osThread(Thread_acceleration), NULL);
+	if(!tid_Thread_acceleration) return (-1);
+	return 0;
+}
+
+
+void Thread_acceleration(void const *argument)
+{
+	while(1)
+	{
+		osSignalWait(0x0001, osWaitForever); // Wait for accelerometer signal
+
+		reading_accel_values(acc);
+		calibration_accel(acc);
+
+		pitch_output = pitch_tilt_angle(acc);
+		roll_output = roll_tilt_angle(acc);
+	}
+}
+
 
 // Initialize accelerometer
 void initialize_accel(void)
@@ -25,8 +62,8 @@ void initialize_accel(void)
 	
 	/* LIS3DSH Data ready interrupt struct */
 	LIS3DSH_DRY.Dataready_Interrupt = LIS3DSH_DATA_READY_INTERRUPT_ENABLED; // Enable DRY interrupt
-  LIS3DSH_DRY.Interrupt_signal = LIS3DSH_ACTIVE_HIGH_INTERRUPT_SIGNAL; // Interrupt Signal Active High (requirements)
-  LIS3DSH_DRY.Interrupt_type = LIS3DSH_INTERRUPT_REQUEST_PULSED; // Interrupt type to be pulsed (requirements)
+  	LIS3DSH_DRY.Interrupt_signal = LIS3DSH_ACTIVE_HIGH_INTERRUPT_SIGNAL; // Interrupt Signal Active High (requirements)
+  	LIS3DSH_DRY.Interrupt_type = LIS3DSH_INTERRUPT_REQUEST_PULSED; // Interrupt type to be pulsed (requirements)
 	
 	LIS3DSH_DataReadyInterruptConfig(&LIS3DSH_DRY);
 	
@@ -71,6 +108,7 @@ float roll_tilt_angle(float *acc)
 	return roll;
 }
 
+
 void calibration_accel(float *acc)
 {
 	// matrix found using Doc 15, Least Square Method
@@ -91,5 +129,3 @@ void calibration_accel(float *acc)
 	acc[1] = new_y;
 	acc[2] = new_z;
 }
-
-
